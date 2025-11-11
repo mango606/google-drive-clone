f75986bda8b055397d5ad4ec5c7184a501b50784
mango606 <yuj08184@kangwon.ac.kr>
2025-11-12 02:02:40 +0900

    feat: Spring Boot 메인 애플리케이션 및 초기 프로젝트 설정 추가

---
+++ src/main/java/com/example/drive/GoogleDriveCloneApplication.java
@@ -0,0 +1,13 @@
+package com.example.drive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleDriveCloneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoogleDriveCloneApplication.class, args);
+    }
+
+}
